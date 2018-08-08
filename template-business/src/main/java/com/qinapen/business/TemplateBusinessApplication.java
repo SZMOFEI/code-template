c9cba5804d5d8f2f3fe354f811f3267b61cd7dff
莫飞 <3084500493@qq.com>
2018-08-08 17:19:59 +0800

    1.项目初始化

---
+++ template-business/src/main/java/com/qinapen/business/TemplateBusinessApplication.java
@@ -0,0 +1,12 @@
+package com.qinapen.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateBusinessApplication.class, args);
+    }
+}
